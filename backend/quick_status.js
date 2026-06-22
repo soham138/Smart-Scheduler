47c540d5c06c28ce57453210486e95155d14324c
soham138 <sohamyadav137@gmail.com>
2026-06-22 18:00:26 +0530

    Uploaded

--- backend/quick_status.js
+++
@@ -1,9 +0,0 @@
-const pool = require('./src/config/db');
-
-async function status() {
-  const res = await pool.query('SELECT COUNT(*) as count FROM timetable');
-  console.log('Total classes in database: ' + res.rows[0].count);
-  pool.end();
-}
-
-status();
